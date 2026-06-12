c150c456322998d89c8f909a5ea5791b0fa2a064
Dmytro Kapusta <info@kapusta.dev>
2026-06-13 00:51:51 +0200

    fix: switch back to native Prisma Client to fix neon websocket connection terminations

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,21 +1,17 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool, neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import ws from 'ws'
-
-neonConfig.webSocketConstructor = ws
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const connectionString = process.env.DATABASE_URL || process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL || ''
-const pool = new Pool({ connectionString })
-// @ts-ignore
-const adapter = new PrismaNeon(pool)
-
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({ adapter })
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL || process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL
+      }
+    }
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
